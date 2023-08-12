16f377655833143b9a96177944358e2be243e5b5
jbarz <jbarz0415@gmail.com>
2023-08-12 19:29:05 -0400

    base web app completed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,46 +11,46 @@ const Portfolio = () => {
     <h1>Portfolio</h1>
     <div className="portfolio-cards">
       <PortfolioCard 
-      title="HowRUFeeling"
-      description="Jounrnaling and mental health self help application."
-      imageUrl="url-to-image-1.jpg"
-      projectLink="https://abisinchan.github.io/verbose-potato/index.html"
-      githubLink="https://github.com/abisinchan/verbose-potato"
+      title="#"
+      description="#"
+      imageUrl="#"
+      projectLink="#"
+      githubLink="#"
       />
       <PortfolioCard 
-      title="Movies Are Us"
-      description="Database application to track the movies you have seen."
-      imageUrl="url-to-image-1.jpg"
-      projectLink="https://arcane-everglades-12126-c502ea749e86.herokuapp.com/"
-      githubLink="https://github.com/sam-stikeleather/Movies-Are-Us"
+      title="#"
+      description="#"
+      imageUrl="#"
+      projectLink="#"
+      githubLink="#"
       />
       <PortfolioCard 
-      title="Note Taker"
-      description="Simple note taking application."
-      imageUrl="url-to-image-1.jpg"
-      projectLink="https://intense-brook-44275-77c54ae7fa56.herokuapp.com/"
-      githubLink="https://github.com/sam-stikeleather/Challenge-11-Notes"
+      title="#"
+      description="#"
+      imageUrl="#"
+      projectLink="#"
+      githubLink="#"
       />
       <PortfolioCard 
-      title="Employee Tracking App"
-      description="Employee database tracking application."
-      imageUrl="url-to-image-1.jpg"
-      projectLink="https://drive.google.com/file/d/1-KPDHpINgnz6Y1BHDLmlUeEF3BWUaMxs/view"
-      githubLink="https://github.com/sam-stikeleather/Challenge-12-Tracker"
+      title="#"
+      description="#"
+      imageUrl="#"
+      projectLink="#"
+      githubLink="#"
       />
       <PortfolioCard 
-      title="9-5 Day Planner"
-      description="Simple realtime, workday planner."
-      imageUrl="url-to-image-1.jpg"
-      projectLink="https://sam-stikeleather.github.io/Challenge-5-Planner/"
-      githubLink="https://github.com/sam-stikeleather/Challenge-5-Planner"
+      title="#"
+      description="#"
+      imageUrl="#"
+      projectLink="#"
+      githubLink="#"
       />
       <PortfolioCard 
-      title="ReadMe Generator"
-      description="Console driven README generator."
-      imageUrl="url-to-image-1.jpg"
-      projectLink="https://drive.google.com/file/d/1bqkP9jRjquXQ6cvE-eN2xcd8I4MFfU-5/view"
-      githubLink="https://github.com/sam-stikeleather/Challenge-9-README"
+      title="#"
+      description="#"
+      imageUrl="#"
+      projectLink="#"
+      githubLink="#"
       />
     </div>
     <Footer />
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -10,9 +10,7 @@ const Welcome = () => {
       <div className="welcome-page">
         <h1>About Me</h1>
         <p>As a new web developer, I am a creative problem-solver with a passion for applying and growing my skills.</p>
-        <p>My background in restaurant management gives me a sense of urgency and an understanding of the necessity that is clear and concise communication. 
-          I am a team player who is always looking to learn and grow. I am excited to bring my skills to a team that is looking to grow and learn together. I am 
-          a Full Stack Web Development Certification holder as of 2023. I am proficient in HTML, CSS, JavaScript, Node.js, Express.js, React.js, and MongoDB.
+        <p>######
         </p>
         <aside className="aside">
           <img src="/headshot2.png" alt="Profile" />
